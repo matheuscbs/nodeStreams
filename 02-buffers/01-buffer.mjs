8e52f2a637cf21847b6ebadba29b7bfd4b732943
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-12-08 16:08:07 -0300

    study: nodejs streams buffers behind the scenes

---
+++ 02-buffers/01-buffer.mjs
@@ -0,0 +1,47 @@
+const buffer = Buffer.alloc(5);
+
+buffer.fill("hi", 0, 2);
+buffer.fill(0x3a, 2, 3); // hexadecimal char code for :
+buffer.fill(0x29, 4, 5); // hexadecimal char code for )
+// error, when it reaches max value, it should be moved to another buffer
+// console.log(buffer.toString())
+// buffer.fill("h", 5, 6);
+
+// console.log({ buffer }); // Buffer log
+const anotherBuffer = Buffer.alloc(6);
+anotherBuffer.set(buffer, buffer.byteOffset);
+anotherBuffer.fill("four", 5, 6);
+console.log(buffer.toString(), buffer, buffer.byteLength);
+console.log(anotherBuffer.toString(), anotherBuffer, anotherBuffer.byteLength);
+
+// or with full data
+const msg = "Hey there!";
+const preAllocated = Buffer.alloc(msg.length, msg);
+
+// same thing of Byffer.from(msg)
+const withBufferFrom = Buffer.from(msg);
+console.log(
+  withBufferFrom.toString(),
+  withBufferFrom,
+  withBufferFrom.byteLength
+);
+console.log(preAllocated.toString(), preAllocated, preAllocated.byteLength);
+
+// -----------
+const str = "Hello World";
+const charCodes = [];
+const bytes = [];
+for (const index in str) {
+  // interger or decimals
+  const code = str.charCodeAt(index);
+  const byteCode = "0x" + Math.abs(code).toString(16);
+  charCodes.push(code);
+  bytes.push(byteCode);
+}
+
+console.log({
+  charCodes,
+  bytes,
+  contentFromCharCodes: Buffer.from(charCodes).toString(),
+  contentFromHexaBytes: Buffer.from(bytes).toString(),
+});
