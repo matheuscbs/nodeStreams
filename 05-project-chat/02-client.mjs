90123d7308ae5d76e8051609cc16b41e1eff36ee
Matheus Cardoso <matheuscbsmsn1994@gmail.com>
2024-12-10 18:49:06 -0300

    study: project chat stream

---
+++ 05-project-chat/02-client.mjs
@@ -0,0 +1,27 @@
+import net from "node:net";
+import readline from "node:readline";
+import { PassThrough, Writable } from "node:stream";
+
+function log(message) {
+  readline.cursorTo(process.stdout, 0);
+  process.stdout.write(message);
+}
+
+const output = Writable({
+  write(chunk, enc, callback) {
+    const { id, message } = JSON.parse(chunk);
+
+    if (message) log(`reply from ${id}: ${message}`);
+    else log(`my username: ${id}\n`);
+
+    log(`type: `);
+    callback(null, chunk);
+  },
+});
+
+const resetChatAfterSent = PassThrough();
+resetChatAfterSent.on("data", (_) => {
+  log("type: ");
+});
+
+process.stdin.pipe(resetChatAfterSent).pipe(net.connect(3000)).pipe(output);
